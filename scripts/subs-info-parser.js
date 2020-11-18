04ce0943924ba293f5669bab45d1b23ef00d88d8
yi_Xu <xjy123456120@163.com>
2020-11-18 12:53:47 +0800

    📝 update subs-info-parser for fit urls

--- scripts/subs-info-parser.js
+++ scripts/subs-info-parser.js
@@ -4,6 +4,12 @@
  */
 
 const { notify } = require('../lib/notify');
+const fs = require('fs');
+const path = require('path');
+const variable_path = path.resolve(__dirname, './variables.json');
+let variables = JSON.parse(fs.readFileSync(variable_path, 'utf-8'))['subs-info-parser'];
+let urls = variables.urls;
+let names = variables.names;
 
 const traffic = num => {
   const s = ['B', 'KB', 'MB', 'GB', 'TB'];
@@ -15,23 +21,34 @@ const traffic = num => {
   return `${idx === 0 ? num : num.toFixed(2)} ${s[idx]}`;
 };
 
-let subs_info_parser = async (raw, { axios, console }, { url, name }) => {
+let subs_info_parser = async (raw, { axios, console }, { name }) => {
   try {
     console.log(`[info]: start fetch subscription-userinfo at ${new Date()}`);
-    const { headers: { 'subscription-userinfo': info } = {} } = await axios.head(url);
-    console.log(`[info]: subscription-userinfo: ${info}`);
-    if (/upload=(\d+)?; download=(\d+)?; total=(\d+)?; expire=(\d+)?/.test(info)) {
+    if (urls.length === 0) throw Error('urls is not defined');
+    if (urls.length != names.length) throw Error('variable need fully defining');
+    for (let i = 0; i < urls.length; i++) {
+      const { headers: { 'subscription-userinfo': info } = {} } = await axios.head(urls[i]);
+      console.log(`[info]: ${names[i]} subscription-userinfo: ${info}`);
+      if (!/upload=(\d+)?; download=(\d+)?; total=(\d+)?; expire=(\d+)?/.test(info)) {
+        console.log(`[info]: urls: ${urls[i]}`);
+        console.log(`[warning]: No found subscription-userinfo in ${names[i]}`);
+        break;
+      }
       let upload = traffic(RegExp.$1 * 1);
-      console.log(`[info]: upload: ${RegExp.$1} = ${upload}`);
+      console.log(`[info]: ${names[i]} upload: ${RegExp.$1} = ${upload}`);
       let download = traffic(RegExp.$2 * 1);
-      console.log(`[info]: download: ${RegExp.$2} = ${download}`);
+      console.log(`[info]: ${names[i]} download: ${RegExp.$2} = ${download}`);
       let total = traffic(RegExp.$3 * 1);
-      console.log(`[info]: total: ${RegExp.$3} = ${total}`);
-      let expire = traffic(RegExp.$4 * 1);
-      console.log(`[info]: expire: ${RegExp.$4} = ${expire}`);
+      console.log(`[info]: ${names[i]} total: ${RegExp.$3} = ${total}`);
+      let expire = new Date(RegExp.$4 * 1000).toLocaleString();
+      console.log(`[info]: ${names[i]} expire: ${RegExp.$4} = ${expire}`);
       let used = traffic(RegExp.$1 * 1 + RegExp.$2 * 1);
-      console.log(`[info]: used: ${RegExp.$1 * 1 + RegExp.$1 * 1} = ${used}`);
-      notify(`${name} successfully update`, `traffic : ${used} | ${total}`, false);
+      console.log(`[info]: ${names[i]} used: ${RegExp.$1 * 1 + RegExp.$1 * 1} = ${used}`);
+      notify(
+        `${names[i]} subscription-userinfo`,
+        `traffic : ${used} | ${total}\nexpire: ${expire}`,
+        false
+      );
     }
     return raw;
   } catch (e) {
